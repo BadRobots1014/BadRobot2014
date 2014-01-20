a7e8c8bae352ffc3b9631270df9ec60d59027797
Isaac Goldthwaite <isaac.goldthwaite@hotmail.com>
2014-01-20 18:13:20 -0500

    added white space..

--- src/com/badrobot/commands/CommandBase.java
+++ src/com/badrobot/commands/CommandBase.java
@@ -22,7 +22,7 @@ public abstract class CommandBase extends Command {
     
     public static IDriveTrain driveTrain;
     public static IShooter shooter;
-
+    
     public static void init() {
         // This MUST be here. If the OI creates Commands (which it very likely
         // will), constructing it during the construction of CommandBase (from
