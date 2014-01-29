1447334df55294e92541a105e299359f8bf5fd19
Steve <steve_popovich@aim.com>
2014-01-29 14:03:16 -0500

    Did more Vision tracking

--- src/com/badrobot/subsystems/VisionTracking.java
+++ src/com/badrobot/subsystems/VisionTracking.java
@@ -5,14 +5,26 @@
 package com.badrobot.subsystems;
 
 import com.badrobot.subsystems.interfaces.IVisionTracking;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
 /**
  *
- * @author Steve
+ * @author Steve and Scott
  */
 public class VisionTracking extends BadSubsystem implements IVisionTracking {
 
-    Camera camera;
+    protected AxisCamera camera;
+    protected Thread t; 
+    protected boolean running;
     
+    public VisionTracking(AxisCamera c)
+    {
+        t = new Thread();
+        t.start();
+        running = true;
+        
+        camera = c;
+        camera.writeResolution(AxisCamera.ResolutionT.k160x120);
+    }
 
     protected void initialize() 
     {
@@ -27,6 +39,16 @@ public class VisionTracking extends BadSubsystem implements IVisionTracking {
     protected void initDefaultCommand() 
     {
     
+    }
+
+    public void run() 
+    {
+        
+    }
+    
+    public void stop() throws InterruptedException
+    {
+        t.join();
     }
     
 }
