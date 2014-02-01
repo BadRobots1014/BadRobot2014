644783acec9e4d74639d65b52a167097d7ba269f
singhalmanu9 <singhalmanu9@gmail.com>
2014-02-01 10:48:10 -0500

    changed TurnRobot and isFinished()

--- src/com/badrobot/commands/TurnRobot.java
+++ src/com/badrobot/commands/TurnRobot.java
@@ -12,53 +12,84 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class TurnRobot extends BadCommand
 {
-    
+    // number of degrees robot needs to turn
     double turnDegrees;
+    // the target for the robot to turn to
+    double target;
     
     public TurnRobot(double degrees)
     {
         requires((Subsystem) driveTrain);
         turnDegrees = degrees;
+        target = driveTrain.getGyro().getAngle() + turnDegrees;
     }
     
     protected void initialize() 
     {
+        driveTrain.tankDrive(0, 0);
     }
 
+    /**
+     * Gets the console identity. Usually the class name.
+     * @return the class name
+     */
     public String getConsoleIdentity() 
     {
         return "TurnRobot";
     }
 
+    /**
+     * Calls this code over and over again.
+     */
     protected void execute() 
     {
-        double target = driveTrain.getGyro().getAngle() + turnDegrees;
-        if(turnDegrees > 0)
+        if(turnDegrees > 0) // if we want a positive turn
         {
+            // if the gyro is not at the target
             if(driveTrain.getGyro().getAngle() < target)
             {
+                // keep turning
                 driveTrain.tankDrive(1.0, -1.0);
             }
-            else if(driveTrain.getGyro().getAngle() > target)
-            {
-                driveTrain.tankDrive(-1.0, 1.0);
-            }
         }
-        else if(turnDegrees < 0)
+        else if(turnDegrees < 0) // if we want a negative turn
         {
+            // if the gyro is not at the target yet
             if(driveTrain.getGyro().getAngle() > target)
             {
+                // keep turning
                 driveTrain.tankDrive(-1.0, 1.0);
-            }
-            else if(driveTrain.getGyro().getAngle() > target)
-            {
-                driveTrain.tankDrive(1.0 , -1.0);
             }
         }
     }
 
+    /**
+     * Stops the drive train and finishes the command.
+     * @return true when the command is finished.
+     */
     protected boolean isFinished() 
     {
+        if(turnDegrees > 0) // if we want a positive turn
+        {
+            // if the gyro is at or past the target
+            if(driveTrain.getGyro().getAngle() > target)
+            {
+                // keep turning
+                driveTrain.tankDrive(0, 0);
+                return true;
+            }
+        }
+        else if(turnDegrees < 0) // if we want a negative turn
+        {
+            // if the gyro is at or past the target
+            if(driveTrain.getGyro().getAngle() < target)
+            {
+                // keep turning
+                driveTrain.tankDrive(0, 0);
+                return true;
+            }
+        }
+        
         return false;
     }
 
