97bdbcac613521b685429d0d21e4ba31b45c05fc
Steve <steve_popovich@aim.com>
2014-03-01 12:08:56 -0500

    Made auto command, command groups, changed dashboard things

---
+++ src/com/badrobot/commands/CheckHotMoveAndShoot.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.badrobot.commands;
+
+import com.badrobot.subsystems.BadSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Utility;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Steve
+ */
+public class CheckHotMoveAndShoot extends BadCommand {
+    
+    Command driveToWall;
+    private long time;
+    boolean supaHotFire, startedDriving;
+    
+    Timer timer;
+    
+    public CheckHotMoveAndShoot()
+    {
+        requires((Subsystem) visionTracking);
+        requires((Subsystem) shooter);
+        requires((Subsystem) driveTrain);
+    }
+
+    protected void initialize() {
+        timer = new Timer();
+        timer.start();
+    }
+
+    public String getConsoleIdentity() {
+        return "CheckHotMoveAndShoot";
+    }
+
+    protected void execute() {
+        
+        if(visionTracking.currentImageIsHot() && !startedDriving)
+        {
+            supaHotFire = true;
+        }
+        else if (!visionTracking.currentImageIsHot() && !startedDriving)
+        {
+            supaHotFire = false;
+        }
+        
+        if (timer.get() > 0.5)
+        {
+            driveToWall = new DriveToWall();
+            driveToWall.start();
+            startedDriving = true;
+        }
+        
+        if (!driveToWall.isRunning() && startedDriving)
+        {
+            if (supaHotFire)
+            {
+                shooter.disengageWinch();
+            }
+            else if (timer.get() > 6)
+            {
+                shooter.disengageWinch();
+            }
+        }
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
